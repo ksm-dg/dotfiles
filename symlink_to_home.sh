01283a8c48de7e04a6d62b1b687550bc395c9366
Kevin S. Martin <arenium@gmail.com>
2024-11-23 18:33:52 -0500

    Updated symlink script to grab the .config/bat/config file

--- symlink_to_home.sh
+++ symlink_to_home.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-find . -maxdepth 1 -type f -name ".*" | xargs -I {} ln -s -f `pwd`/{} ~/{}
+# Find all dotfiles and .config/foo/config files and create symlinks to them from home
+# Be careful to test modifications to this command with an echo in the xargs first!
+# Also be careful to honor the ln argument order: target linkname
+#                                                   |       ^ ~/ OR a test directory
+#                                                   ^ this dir
+# First part of find an idiom to ignore the .git directory
+find . -not \( -path "./.git" -type d -prune \) -type f -maxdepth 3 \
+    ! -name ".DS_Store" -and -name ".*" -or -name "config" | 
+   sed -e 's/^\.\///' | xargs -I {} ln -s -f `pwd`/{} ~/{}
